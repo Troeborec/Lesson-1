e53e6f7895ee56d126dc60ec8609579283bff7b2
Troeborec <defied_jeff@mail.ru>
2022-11-30 06:14:04 +1200

    Задача 7

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@
     public static void main(String[] args) {
         var boxer1 = 78.2;
         var boxer2 = 82.7;
-        var boxWeight = boxer1 + boxer2;
-        System.out.println(boxWeight);
-        var differenceWeight = boxer1 - boxer2;
+        var differenceWeight = boxer2 - boxer1;
         System.out.println(differenceWeight);
+        var differenceWeight2 = boxer2 % boxer1;
+        System.out.println(differenceWeight2);
     }
 }
