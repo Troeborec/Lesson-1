d9cc5aaf649b4d35cbdbc9db618d22143f24a3b2
Troeborec <defied_jeff@mail.ru>
2022-11-30 07:02:31 +1200

    Задача 8

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
-    public class Main {
+public class Main {
     public static void main(String[] args) {
-        var boxer1 = 78.2;
-        var boxer2 = 82.7;
-        var boxWeight = boxer1 + boxer2;
-        System.out.println(boxWeight);
-        var differenceWeight = boxer1 - boxer2;
-        System.out.println(differenceWeight);
+        var timeWork = 640;
+        var oneEmployee = 8;
+        var allEmployee = timeWork / oneEmployee;
+        System.out.println("Всего работников в компании " + allEmployee + " человек");
+        var allpeople = allEmployee + 94;
+        System.out.println(allpeople);
+        var needhours = timeWork / allpeople;
+        System.out.println(needhours);
+        System.out.println("Если в компании работает " + allpeople + " человека, то всего " + needhours + " часов работы может быть поделено между сотрудниками");
     }
 }
