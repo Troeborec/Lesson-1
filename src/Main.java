631da3ec008da3de35ba4238438e5a48d054d25e
Troeborec <defied_jeff@mail.ru>
2022-11-30 06:11:09 +1200

    Задача 6

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
-public class Main {
+    public class Main {
     public static void main(String[] args) {
-       var frog = 3.5;
-       System.out.println(frog);
-       frog = frog * 10;
-       System.out.println(frog);
-       frog = frog / 3.5;
-       System.out.println(frog);
-       frog = frog + 4;
-       System.out.println(frog);
-
+        var boxer1 = 78.2;
+        var boxer2 = 82.7;
+        var boxWeight = boxer1 + boxer2;
+        System.out.println(boxWeight);
+        var differenceWeight = boxer1 - boxer2;
+        System.out.println(differenceWeight);
     }
 }
